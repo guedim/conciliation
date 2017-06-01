9013cdd93ab032dd8619acadf9e14d189d5b6742
Mario Guerrero <guedim@gmail.com>
2017-06-01 12:08:17 -0500

    Prueba

--- src/main/java/com/mario/movilred/conciliation/ConciliationApplication.java
+++ src/main/java/com/mario/movilred/conciliation/ConciliationApplication.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 //@EnableAutoConfiguration(exclude={CassandraDataAutoConfiguration.class})
 public class ConciliationApplication {
 
+  
   public static void main(String[] args) {
     SpringApplication.run(ConciliationApplication.class, args);
   }
